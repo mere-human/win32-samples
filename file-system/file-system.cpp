8c6e5253af4234362dd5ba710474b6a336e994d8
mere-human <9664141+mere-human@users.noreply.github.com>
2021-02-27 17:44:08 +0200

    Use list box for path info

--- file-system/file-system.cpp
+++ file-system/file-system.cpp
@@ -5,6 +5,7 @@
 #include "file-system.h"
 #include <shlwapi.h>
 #include <array>    // size
+#include <windowsx.h>   // ListBox
 
 #define MAX_LOADSTRING 100
 
@@ -21,41 +22,41 @@ INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    CheckPathDlgProc(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_FILESYSTEM, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_FILESYSTEM, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FILESYSTEM));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FILESYSTEM));
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -67,23 +68,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_FILESYSTEM));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_FILESYSTEM);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_FILESYSTEM));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_FILESYSTEM);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -98,20 +99,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -126,156 +127,148 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_FILE_PATH:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_PATH_DIALOG), hWnd, CheckPathDlgProc);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_FILE_PATH:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_PATH_DIALOG), hWnd, CheckPathDlgProc);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+void FillListBox(HWND hDlg)
+{
+	TCHAR path[MAX_PATH] = {};
+
+	HWND hEdit = GetDlgItem(hDlg, IDC_PATH_EDIT);
+	if (hEdit)
+		GetWindowText(hEdit, path, std::size(path));
+	if (_tcslen(path) == 0)
+	{
+		if (GetModuleFileName(NULL, path, std::size(path)) != 0)
+			SetWindowText(hEdit, path);
+	}
+
+	HWND hList = GetDlgItem(hDlg, IDC_LIST_PATH_INFO);
+	ListBox_ResetContent(hList);
+
+	TCHAR buffer[MAX_PATH] = {};
+	TCHAR buffer2[MAX_PATH] = {};
+
+	using PathQuery = BOOL(__stdcall*)(LPCTSTR);
+	struct PathQueryPack
+	{
+		const TCHAR* name;
+		PathQuery func;
+	};
+	PathQueryPack pathQueries[] = {
+		{_T("PathIsDirectory"), &PathIsDirectory},
+		{_T("PathFileExists"), &PathFileExists},
+		{_T("PathIsNetworkPath"), &PathIsNetworkPath},
+		{_T("PathIsRelative"), &PathIsRelative},
+		{_T("PathIsRoot"), &PathIsRoot},
+		{_T("PathIsUNC"), &PathIsUNC},
+		{_T("PathIsURL"), &PathIsURL},
+	};
+
+	for (auto&& query : pathQueries)
+	{
+		_stprintf_s(buffer, _T("%s: %d"), query.name, query.func(path));
+		ListBox_AddString(hList, buffer);
+	}
+
+	TCHAR** lppPart = { NULL };
+	if (GetFullPathName(path, std::size(path), buffer2, lppPart) != 0)
+		_stprintf_s(buffer, _T("GetFullPathName: %s"), buffer2);
+	else
+		_stprintf_s(buffer, _T("GetFullPathName: ?"));
+	ListBox_AddString(hList, buffer);
+
+	if (PathCanonicalize(buffer2, path))
+		_stprintf_s(buffer, _T("PathCanonicalize: %s"), buffer2);
+	else
+		_stprintf_s(buffer, _T("PathCanonicalize: ?"));
+	ListBox_AddString(hList, buffer);
+
+	if (GetCurrentDirectory(std::size(path), path) != 0)
+		_stprintf_s(buffer, _T("GetCurrentDirectory: %s"), path);
+	else
+		_stprintf_s(buffer, _T("GetCurrentDirectory: ?"));
+	ListBox_AddString(hList, buffer);
 }
 
 // Message handler for "Check Path" dialog.
-// TODO: use a list box?
 INT_PTR CALLBACK CheckPathDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    static TCHAR buffer[MAX_PATH] = {};
-    static TCHAR buffer2[MAX_PATH] = {};
-    static TCHAR path[MAX_PATH] = {};
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		FillListBox(hDlg);
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK)
-        {
-            HWND hChild = GetDlgItem(hDlg, IDC_PATH_EDIT);
-            if (hChild)
-                GetWindowText(hChild, path, std::size(path));
-            using PathQuery = BOOL(__stdcall *)(LPCTSTR);
-            struct PathQueryPack
-            {
-                int id;
-                const TCHAR* name;
-                PathQuery func;
-            };
-            PathQueryPack pathQueries[] = {
-                {IDC_STATIC_ISDIR, _T("PathIsDirectory"), &PathIsDirectory},
-                {IDC_STATIC_FILE_EXISTS, _T("PathFileExists"), &PathFileExists},
-                {IDC_STATIC_IS_NET_PATH, _T("PathIsNetworkPath"), &PathIsNetworkPath},
-                {IDC_STATIC_IS_REL, _T("PathIsRelative"), &PathIsRelative},
-                {IDC_STATIC_IS_ROOT, _T("PathIsRoot"), &PathIsRoot},
-                {IDC_STATIC_IS_UNC, _T("PathIsUNC"), &PathIsUNC},
-                {IDC_STATIC_IS_URL, _T("PathIsURL"), &PathIsURL},
-            };
-            for (auto&& query : pathQueries)
-            {
-                HWND hChild = GetDlgItem(hDlg, query.id);
-                if (hChild)
-                {
-                    _stprintf_s(buffer, _T("%s: %d"), query.name, query.func(path));
-                    SetWindowText(hChild, buffer);
-                }
-            }
-            hChild = GetDlgItem(hDlg, IDC_STATIC_FULL_PATH);
-            if (hChild)
-            {
-                TCHAR** lppPart = { NULL };
-				if (GetFullPathName(path, std::size(path), buffer2, lppPart) != 0)
-                {
-                    _stprintf_s(buffer, _T("GetFullPathName: %s"), buffer2);
-                }
-                else
-                {
-                    _stprintf_s(buffer, _T("GetFullPathName: ?"));
-                }
-            }
-            hChild = GetDlgItem(hDlg, IDC_STATIC_PATH_CANON);
-            if (hChild)
-            {
-                if (PathCanonicalize(buffer2, path))
-                {
-                    _stprintf_s(buffer, _T("PathCanonicalize: %s"), buffer2);
-                }
-                else
-                {
-                    _stprintf_s(buffer, _T("PathCanonicalize: ?"));
-                }
-                SetWindowText(hChild, buffer);
-            }
-            hChild = GetDlgItem(hDlg, IDC_STATIC_CURR_DIR);
-            if (hChild)
-            {
-                if (GetCurrentDirectory(std::size(path), path) != 0)
-                {
-                    _stprintf_s(buffer, _T("GetCurrentDirectory: %s"), path);
-                }
-                else
-                {
-                    _stprintf_s(buffer, _T("GetCurrentDirectory: ?"));
-                }
-                SetWindowText(hChild, buffer);
-            }
-        }
-        else if (LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			FillListBox(hDlg);
+		}
+		else if (LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
 
--- file-system/resource.h
+++ file-system/resource.h
@@ -14,17 +14,8 @@
 #define IDR_MAINFRAME                   128
 #define IDD_PATH_DIALOG                 129
 #define IDC_PATH_EDIT                   1000
-#define IDC_STATIC_ISDIR                1002
-#define IDC_STATIC_FILE_EXISTS          1003
-#define IDC_STATIC_IS_NET_PATH          1004
-#define IDC_STATIC_IS_REL               1005
-#define IDC_STATIC_IS_ROOT              1006
-#define IDC_STATIC_IS_UNC               1008
-#define IDC_STATIC_IS_URL               1009
-#define IDC_STATIC_PATH_CANON           1010
-#define IDC_STATIC_CURR_DIR             1011
-#define IDC_STATIC_CURR_DIR2            1012
-#define IDC_STATIC_FULL_PATH            1012
+#define IDC_LIST_                       1003
+#define IDC_LIST_PATH_INFO              1003
 #define ID_FILE_PATH                    32771
 #define IDM_FILE_PATH                   32772
 #define IDC_STATIC                      -1
@@ -36,7 +27,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32773
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
